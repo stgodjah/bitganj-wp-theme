b45ba37f078d90aef737861745d4183d6521b797
stgodjah <godjah@bitganj.website>
2019-02-23 14:34:42 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+# bitganj-wp-theme
+WordPress theme
+=======
+# bitganj-wp-theme
+>>>>>>> refs/remotes/origin/master
+=======
+# bitganj-wp-theme
+>>>>>>> refs/remotes/github/master
---
+++ twentyseventeen-child/template-parts/footer/site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+<center>Designed by god Jah!</center>
+</div><!-- .site-info -->
